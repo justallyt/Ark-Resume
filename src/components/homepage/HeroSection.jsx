15b46b83e97f3a1d9d933fd13510a2174f55661b
justallyt <albertjunk2030@gmail.com>
2024-06-07 16:57:01 +0300

    Update: Hero Section

---
+++ src/components/homepage/HeroSection.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+import hero from "../../assets/hero1.jpg"
+import hero2 from "../../assets/hero2.jpg"
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+              <div className="inner-row">
+                         <div className="hero-section-content">
+                                     <div className="hero-title">
+                                            <h1>Crafting Resumes that Help You Find your Dream Job.</h1>
+                                     </div>
+                                     <div className="hero-wrap">
+                                                <div className="hero-image">
+                                                          <img src={hero} alt="" />
+                                                </div>
+                                                <div className="hero-texts">
+                                                          <p>We help you stand out from the crowd and confidently ace interviews by crafting winning resumes, cover letters, LinkedIn profiles, biographies, and elevator pitches that get you noticed and interviewed.</p>
+                                                          <Link to={'/'}>Explore Services</Link>
+
+                                                          <img src={hero2} alt="" />
+                                                </div>
+                                     </div>
+                         </div>
+              </div>
+    </div>
+  )
+}
+
+export default HeroSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import Header from "../components/common/navigation/Header"
+import AboutSection from "../components/homepage/AboutSection"
+import HeroSection from "../components/homepage/HeroSection"
+import "../css/home.css"
 
 const Home = () => {
   return (
     <>
           <Header />
+          <HeroSection />
+          <AboutSection />
     </>
   )
 }
