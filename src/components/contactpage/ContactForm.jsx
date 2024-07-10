db01a0a18c1d7a59cbfb8066b23ef0924bdf31e7
justallyt <albertjunk2030@gmail.com>
2024-07-10 17:35:38 +0300

    Updates

--- src/components/contactpage/ContactForm.jsx
+++ src/components/contactpage/ContactForm.jsx
@@ -1,31 +1,71 @@
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import axios from "axios"
 
 const ContactForm = () => {
+  const API_PATH = import.meta.env.VITE_CONTACT_FORM_URL ;
+  const { register, handleSubmit , formState: { errors }, reset} = useForm();
+  const [btnMsg, setBtnMsg] = useState("Submit Message");
+  const [serverMsg, setServerMsg] = useState("")
+
+  const submitForm = (form_data) => {
+    setBtnMsg("Sending...");
+    try {
+      axios.post(API_PATH, {
+           form_data: form_data
+          }).then(res => {
+                  setServerMsg(res.data.message);
+                  reset();
+            }).finally(() =>{
+                 setBtnMsg('Sent')
+                 setTimeout(() => {
+                      setBtnMsg('Submit Message');
+                      setServerMsg('')
+                 }, 5000)
+            })
+ } catch (error) {
+      setBtnMsg('Submit Message');
+      setServerMsg('')
+ }
+  }
   return (
        <div className="contact-form">
                 <h2>Get in Touch</h2>
                 <p>You can reach us at any time.</p>
-                <form>
+                <form onSubmit={handleSubmit(submitForm)}>
                          <div className="form-row split">
                                     <div className="form-row-column">
-                                              <input type="text" className="form-control" placeholder="First name"  />
+                                              <div className="input-row">
+                                                        <input type="text" {...register("firstname", { required: "Firstname input is required"})} className="form-control" placeholder="First name"  />
+                                              </div>
+                                              <span className="error">{errors.firstname && errors.firstname.message}</span>
                                     </div>
                                     <div className="form-row-column">
-                                              <input type="text" className="form-control" placeholder="Last name" />
+                                               <div className="input-row">
+                                                        <input type="text" {...register("lastname", { required: "Lastname input is required"})} className="form-control" placeholder="Last name" />
+                                               </div>
+                                                <span className="error">{errors.lastname && errors.lastname.message}</span>
                                     </div>
                          </div>
+                         <input type="text" {...register("honey")} className="honey-special"/>
                          <div className="form-row">
-                                     <input type="email" className="form-control" placeholder="Your email" />
+                                     <input type="email" {...register("email", { required: 'Email input is required'})} className="form-control" placeholder="Your email" />
+                                     <span className="error">{errors.email && errors.email.message}</span>
                          </div>
                          <div className="form-row">
-                                    <input type="number" placeholder="Phone number" className="form-control" pattern="+[0,9]"  />
+                                    <input type="number" {...register("phone", { required: "Phone number input is required"})} placeholder="Phone number" className="form-control" pattern="+[0,9]"  />
+                                    <span className="error">{errors.phone && errors.phone.message}</span>
                          </div>
                          <div className="form-row">
-                                   <textarea placeholder="How can we help?"></textarea>
+                                   <textarea placeholder="How can we help?" {...register("msg", { required: "Message inquiry input is required"})}></textarea>
+                                   <span className="error">{errors.msg && errors.msg.message}</span>
                          </div>
 
                          <div className="form-btn">
-                                    <button>Submit</button>
+                                    <button type="submit">{btnMsg}</button>
                          </div>
+
+                         <p className="server-msg">{serverMsg}</p>
                 </form>
        </div>
   )
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,6 +1,35 @@
 import Navbar from "../components/common/navigation/Navbar"
 import Footer from "../components/common/Footer"
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import axios from "axios"
+
 const Book = () => {
+  const API_PATH = import.meta.env.VITE_BOOKING_FORM_URL ;
+  const { register, handleSubmit , formState: { errors }, reset} = useForm();
+  const [btnMsg, setBtnMsg] = useState("Book Now");
+  const [serverMsg, setServerMsg] = useState("")
+
+  const submitBooking = (form_data) => {
+    setBtnMsg("Booking...");
+    try {
+      axios.post(API_PATH, {
+           form_data: form_data
+          }).then(res => {
+                  setServerMsg(res.data.message);
+                  reset();
+            }).finally(() =>{
+                 setBtnMsg('Booked')
+                 setTimeout(() => {
+                      setBtnMsg('Book Now');
+                      setServerMsg('')
+                 }, 5000)
+            })
+ } catch (error) {
+      setBtnMsg('Book Now');
+      setServerMsg('')
+ }
+  }
   return (
     <>
           <Navbar />
@@ -13,49 +42,66 @@ const Book = () => {
                                          </div>
 
                                          <div className="booking-form">
-                                                   <form action="">
+                                                   <form onSubmit={handleSubmit(submitBooking)}>
                                                              <div className="form-row split">
                                                                        <div className="form-column">
-                                                                                 <label htmlFor="firstname">Firstname</label>
-                                                                                 <input type="text" className="form-control" placeholder="First name" />
+                                                                                 <div className="input-row">
+                                                                                        <label htmlFor="firstname">Firstname</label>
+                                                                                        <input type="text" className="form-control" placeholder="First name" {...register("firstname", { required: "Firstname input is required"})} />
+                                                                                 </div>
+                                                                                 <span className="error">{errors.firstname && errors.firstname.message}</span>
                                                                        </div>
                                                                        <div className="form-column">
-                                                                                 <label htmlFor="lastname">Lastname</label>
-                                                                                 <input type="text" className="form-control" placeholder="Last name" />
+                                                                                <div className="input-row">
+                                                                                            <label htmlFor="lastname">Lastname</label>
+                                                                                            <input type="text" className="form-control" placeholder="Last name" {...register("lastname", { required: "Lastname input is required"})} />
+                                                                                </div>
+                                                                                <span className="error">{errors.lastname && errors.lastname.message}</span>
                                                                        </div>
                                                              </div>
                                                              <div className="form-row split">
                                                                          <div className="form-column">
-                                                                                  <label htmlFor="email">Email</label>
-                                                                                  <input type="email" className="form-control" placeholder="Email address" />
+                                                                                <div className="input-row">
+                                                                                          <label htmlFor="email">Email</label>
+                                                                                          <input type="email" {...register("email", { required: "Email address input is required"})} className="form-control" placeholder="Email address" />
+                                                                                          <span className="error">{errors.email && errors.email.message}</span>
+                                                                                </div>
                                                                          </div>
                                                                          <div className="form-column">
-                                                                                  <label htmlFor="phone">Phone Number</label>
-                                                                                  <input type="number" className="form-control" placeholder="+254 712345678" pattern="+[0,9]" />
+                                                                                  <div className="input-row">
+                                                                                            <label htmlFor="phone">Phone Number</label>
+                                                                                            <input type="number" className="form-control" placeholder="+254 712345678" pattern="+[0,9]" {...register("phone", { required: "Phone number input is required"})} />
+                                                                                  </div>
+                                                                                  <span className="error">{errors.phone && errors.phone.message}</span>
                                                                          </div>
                                                              </div>
+                                                             <input type="text" {...register("honey")} className="honey-special"/>
                                                              <div className="form-row">
                                                                         <label htmlFor="date">Schedule a date of appointment</label>
-                                                                        <input type="date" className="form-control" />
+                                                                        <input type="date" className="form-control" {...register("appointment_date", {required: "Appointment date is required"})} />
+                                                                        <span className="error">{errors.appointment_date && errors.appointment_date.message}</span>
                                                              </div>
                                                              <div className="form-row">
                                                                         <label htmlFor="service">Type of Service</label>
-                                                                        <select name="" className="form-control">
+                                                                        <select {...register("service_type", { required: "Please select a service"})} className="form-control">
                                                                                   <option value="">Choose the service you require</option>
                                                                                   <option value="Modern Resume">Modern Resume</option>
                                                                                   <option value="Cover Letter">Cover Letter</option>
                                                                                   <option value="Linkedin Optimization">LinkedIn Optimization</option>
                                                                                   <option value="Job Search Assistance">Job Search Assistance</option>
                                                                         </select>
+                                                                        <span className="error">{errors.service_type && errors.service_type.message}</span>
                                                              </div>
                                                              <div className="form-row">
                                                                       <label htmlFor="info">Extra Info</label>
-                                                                       <textarea name="" placeholder="Any extra info you'd like to add"></textarea>
+                                                                       <textarea {...register("info")} placeholder="Any extra info you'd like to add"></textarea>
                                                              </div>
 
                                                              <div className="form-btn">
-                                                                        <button type="submit">Book Now</button>
+                                                                        <button type="submit">{btnMsg}</button>
                                                              </div>
+
+                                                             <div className="server-msg">{serverMsg}</div>
                                                    </form>
                                          </div>
                               </div>
